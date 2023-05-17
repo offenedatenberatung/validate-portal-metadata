9c7bc3370f016b71ff68f9000ccc32cd5994ba34
Mila Frerichs <mila.frerichs@gmail.com>
2023-05-17 20:54:25 +0200

    feat: add more stats at the top level
    add more tests, go to version 1 and update readme

--- dcat_ap_de_validator/commands/portal.py
+++ dcat_ap_de_validator/commands/portal.py
@@ -12,14 +12,21 @@ def execute(args):
         portal = CKAN(args.url)
     package_list = portal.packages()
     portal_title = portal.get_title()
+    validation_result = {"portal": portal_title, "warnings": 0, "errors": 0, "infos": 0, "valid_datasets": 0,"results": []}
     results = []
 
     for package_name in package_list:
         result = validate(portal.package_metadata_url(package_name))
         # count errors and warnings, successes
+        validation_result["warnings"] += result["warning"]
+        validation_result["errors"] += result["error"]
+        validation_result["infos"] += result["info"]
+        if result.get("valid", False):
+            validation_result["valid_datasets"] += 1
         results.append({"package": package_name, "url": portal.package_url(package_name), "result": result})
 
+    validation_result["results"] = results
     with open(f"{portal_title}.json", "w") as f:
-        json.dump(results, f)
+        json.dump(validation_result, f)
 
-    return results
+    return validation_result
