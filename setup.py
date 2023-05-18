1e5539b10bd1f83d1bb2f77c85a21a152b58313f
Mila Frerichs <mila.frerichs@gmail.com>
2023-05-18 12:53:52 +0200

    feat: try adding release workflow
    to allow installation from gh

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
         name="dcat_ap_de_validator",
-        version="1.0.0",
+        version="1.0.1",
         author="Mila Frerichs",
         author_email="mila@offenedatenberatung.de",
         description="Validator for DCAT-AP.de Metadatasets using the EU Validator Endpoint",
-        url="",
+        url="https://github.com/offenedatenberatung/validate-portal-metadata",
         packages=find_packages(),
         install_requires=[
                 "requests==2.30.0",
@@ -18,7 +18,7 @@ setup(
         },
         classifiers=[
                     "Programming Language :: Python :: 3",
-                    "License :: OSI Approved :: MIT License",
+                    "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
                     "Operating System :: OS Independent",
         ],
         python_requires=">=3.9",
