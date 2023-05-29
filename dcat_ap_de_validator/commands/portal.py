f58f057fd87e07f3a4f179e5e5897af0a7650f56
Mila Frerichs <mila.frerichs@gmail.com>
2023-05-29 22:46:14 +0200

    feat: add stats and progress through rich
    adding more info to the console on progress and also displaying the
    resulting stats

--- dcat_ap_de_validator/commands/portal.py
+++ dcat_ap_de_validator/commands/portal.py
@@ -1,9 +1,14 @@
 import json
+
+from rich.console import Console
+from rich.progress import track
+
 from dcat_ap_de_validator.portals import Portal, CKAN, DKAN
 from dcat_ap_de_validator.metadata.validate import validate
 
 
 def execute(args):
+    console = Console()
     portal_type = str(args.portal_type)
     portal = Portal(args.url)
     if portal_type.lower() == "dkan":
@@ -12,10 +17,12 @@ def execute(args):
         portal = CKAN(args.url)
     package_list = portal.packages()
     portal_title = portal.get_title()
-    validation_result = {"portal": portal_title, "warnings": 0, "errors": 0, "infos": 0, "valid_datasets": 0,"results": []}
+    validation_result = {"portal": portal_title, "warnings": 0, "errors": 0, "infos": 0, "valid_datasets": 0, "results": []}
     results = []
 
-    for package_name in package_list:
+    console.rule("[bold red]Metadaten download")
+    console.print(f"Wir haben {len(package_list)} Daten gefunden.", style="bold gold3")
+    for package_name in track(package_list, description="Metadaten validieren..."):
         result = validate(portal.package_metadata_url(package_name))
         # count errors and warnings, successes
         validation_result["warnings"] += result["warning"]
@@ -25,6 +32,11 @@ def execute(args):
             validation_result["valid_datasets"] += 1
         results.append({"package": package_name, "url": portal.package_url(package_name), "result": result})
 
+    console.rule("[bold red]Validierungsergebnisse")
+    console.print("Valid Datasets:", validation_result["valid_datasets"], style="bold green4")
+    console.print("Infos:", validation_result["infos"], style="bold grey39")
+    console.print("Warnings:", validation_result["warnings"], style="bold gold3")
+    console.print("Errors:", validation_result["errors"], style="bold red")
     validation_result["results"] = results
     with open(f"{portal_title}.json", "w") as f:
         json.dump(validation_result, f)
